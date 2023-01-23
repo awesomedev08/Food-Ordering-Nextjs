2ea576809d742b94312f2695349e193876f01148
Jfnrosimo <j.rosimo098.com>
2023-01-23 14:27:07 +0800

    Feat; finalize footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -6,30 +6,34 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <Image src="img/bg.png" alt="footer background" fill />
+        <Image src="/img/bg.png" alt="footer background" fill />
       </div>
       <div className={styles.item}>
-        <h2 className={styles.card}>J's PIZZA. WELL BAKED SLICE OF PIZZA.</h2>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            J's PIZZA. WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
         <div className={styles.card}>
           <h2 className={styles.title}>FIND OUR RESTAURANTS</h2>
           <p className={styles.text}>
             1212 R. P.T. La Trinidad Road #007.
-            <span>La Trinidad, 1122</span>
+            <span className={styles.subText}>La Trinidad, 1122</span>
             <span>(072) 424-9898</span>
           </p>
           <p className={styles.text}>
             1212 R. P.T. La Trinidad Road #007.
-            <span>La Trinidad, 1122</span>
+            <span className={styles.subText}>La Trinidad, 1122</span>
             <span>(072) 424-9898</span>
           </p>
           <p className={styles.text}>
             1212 R. P.T. La Trinidad Road #007.
-            <span>La Trinidad, 1122</span>
+            <span className={styles.subText}>La Trinidad, 1122</span>
             <span>(072) 424-9898</span>
           </p>
           <p className={styles.text}>
             1212 R. P.T. La Trinidad Road #007.
-            <span>La Trinidad, 1122</span>
+            <span className={styles.subText}>La Trinidad, 1122</span>
             <span>(072) 424-9898</span>
           </p>
         </div>
@@ -37,11 +41,11 @@ const Footer = () => {
           <h2 className={styles.title}>WORKING HOURS</h2>
           <p className={styles.text}>
             MONDAY - SATURDAY
-            <span>10:00 - 24:00</span>
+            <span className={styles.subText}>10:00 - 24:00</span>
           </p>
           <p className={styles.text}>
             SUNDAY
-            <span>10:00 - 21:00</span>
+            <span className={styles.subText}>10:00 - 21:00</span>
           </p>
         </div>
       </div>
