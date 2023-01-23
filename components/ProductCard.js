0de367e843adffea28798d29f9aeb116719c330a
Jfnrosimo <j.rosimo098.com>
2023-01-23 13:33:41 +0800

    Feat: added product list and card with styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,7 +1,18 @@
-import React from "react";
+//Import UI
+import styles from "styles/ProductCard.module.css";
+import Image from "next/image";
 
 const ProductCard = () => {
-  return <div>ProductCard</div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="250" height="250" />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <p className={styles.price}>₱499</p>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum?
+      </p>
+    </div>
+  );
 };
 
 export default ProductCard;
