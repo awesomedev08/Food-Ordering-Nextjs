d157c538b314f37eb98fec054559b5a572807065
Jfnrosimo <j.rosimo098.com>
2023-01-23 14:35:43 +0800

    Feat: setup individual page

---
+++ pages/product/[id].js
@@ -0,0 +1,9 @@
+//Import UI
+import styles from "styles/ProductId.module.css";
+import Image from "next/image";
+
+const ProductId = () => {
+  return <div className={styles.container}>ProductId</div>;
+};
+
+export default ProductId;
