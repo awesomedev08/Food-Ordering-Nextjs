2fff149d8f9bf2675a1c3a476cd7fd78aefa8a41
Jfnrosimo <j.rosimo098.com>
2023-01-25 18:54:46 +0800

    Feat: added get and post functionality

--- util/mongo.js
+++ util/mongo.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
-if (!MONGODB_URL) {
+if (!MONGO_URL) {
   throw new Error(
     "Please define the MONGODB_URL environment variable inside .env.local"
   );
@@ -29,7 +29,7 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
       return mongoose;
     });
   }
