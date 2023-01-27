682e40117da5207189faa2b7cd235a8347aad3d4
Jfnrosimo <j.rosimo098.com>
2023-01-27 11:27:42 +0800

    Fix: bad req on first load of page

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,14 @@ export default function Home({ productList }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products");
-
-  return {
-    props: {
-      productList: res.data,
-    },
-  };
+  try {
+    const res = await axios.get("http://localhost:3000/api/products");
+    return {
+      props: {
+        productList: res.data,
+      },
+    };
+  } catch (error) {
+    return { props: { productList: [] } };
+  }
 };
