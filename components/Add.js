a1d217bb35d678ef26612db02cc10c147b2cfa85
Jfnrosimo <j.rosimo098.com>
2023-01-31 09:03:58 +0800

    Fix: add button and file

--- components/Add.js
+++ components/Add.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { useRouter } from "next/router";
 
-const Add = () => {
-  return <div>Hello</div>;
+//Import UI
+import styles from "../styles/Add.module.css";
+
+const Add = ({ setClose }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <span className={styles.close} onClick={() => setClose(true)}>
+          X
+        </span>
+        <h1>Add a new Pizza</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choose an Image</label>
+          <input type="file" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
